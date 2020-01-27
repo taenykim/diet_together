eaecad51a5502d0eda450d94174a8b322b20d663
taeeun kim <kimtaeeeny@gmail.com>
2020-01-27 13:08:28 +0900

    react-helmet -> post page 적용

--- front/pages/post.js
+++ front/pages/post.js
@@ -2,11 +2,38 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import proptypes from 'prop-types'
 import { LOAD_POST_REQUEST } from '../reducers/post'
+import Helmet from 'react-helmet'
 
 const Post = ({ id }) => {
   const { singlePost } = useSelector(state => state.post)
   return (
     <>
+      <Helmet
+        title={`${singlePost.User.nickname}님의 게시글`}
+        description={singlePost.content}
+        meta={[
+          {
+            name: 'description',
+            content: singlePost.content
+          },
+          {
+            property: 'og:title',
+            content: `${singlePost.User.nickname}님의 게시글`
+          },
+          {
+            property: 'og:description',
+            content: singlePost.content
+          },
+          {
+            property: 'og:image',
+            content: singlePost.Images[0] && `http://localhost:3065/${singlePost.Images[0].src}`
+          },
+          {
+            property: 'og:url',
+            content: `http://localhost:3060/post/${id}`
+          }
+        ]}
+      />
       <div>{singlePost.content}</div>
       <div>{singlePost.User.nickname}</div>
       <div>
