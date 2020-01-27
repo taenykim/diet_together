0f2e148abece1da968a566ff56138c6dd13bcff3
taeeun kim <kimtaeeeny@gmail.com>
2020-01-27 13:35:25 +0900

    _document 헬멧 적용

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import AppLayout from '../components/AppLayout'
-import Head from 'next/head'
+import Helmet from 'react-helmet'
 import withRedux from 'next-redux-wrapper'
 import withReduxSaga from 'next-redux-saga'
 import { createStore, compose, applyMiddleware } from 'redux'
@@ -11,28 +11,62 @@ import rootSaga from '../sagas'
 import createSagaMiddleware from 'redux-saga'
 import { LOAD_USER_REQUEST } from '../reducers/user'
 import axios from 'axios'
+import { Container } from 'next/app'
 
 const _app = ({ Component, store, pageProps }) => {
   return (
-    <Provider store={store}>
-      <Head>
-        <title>DietTogether</title>
-        <link
-          rel="stylesheet"
-          type="text/css"
-          charset="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+    <Container>
+      <Provider store={store}>
+        <Helmet
+          title="DietTogether"
+          htmlAttributes={{ lang: 'ko' }}
+          meta={[
+            {
+              charset: 'UTF-8'
+            },
+            {
+              name: 'viewport',
+              content:
+                'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes,viewport-fit=cover'
+            },
+            {
+              'http-equiv': 'X-UA-Compatible',
+              content: 'IE=edge'
+            },
+            {
+              name: 'description',
+              content: 'DietTogether'
+            },
+            {
+              name: 'og:title',
+              content: 'DietTogether'
+            },
+            {
+              name: 'og:description',
+              content: 'DietTogether'
+            },
+            {
+              property: 'og:type',
+              content: 'website'
+            }
+          ]}
+          link={[
+            {
+              rel: 'stylesheet',
+              href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css'
+            },
+            {
+              rel: 'stylesheet',
+              href:
+                'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css'
+            }
+          ]}
         />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-        />
-      </Head>
-      <AppLayout>
-        <Component {...pageProps} />
-      </AppLayout>
-    </Provider>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </Provider>
+    </Container>
   )
 }
 
